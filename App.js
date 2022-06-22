b7d2f9878bd585b2c1e438b0e6c3b8690f87f38b
Theadora Williams <theadorabledeveloper@gmail.com>
2022-06-22 01:42:38 -0400

    Added very early steps in app

--- App.js
+++ App.js
@@ -1,15 +1,68 @@
 import { View, Text } from "react-native";
+import NavigationBar from 'react-native-navbar';
+
+const styles = {
+    container: {
+	flex: 1,
+	paddingTop: 32,
+	padding: 2,
+	borderRadius: 32,
+    },
+    chatComp: {
+	flex: 1,
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	borderWidth: 1,
+	maxHeight: 64,
+	
+    }
+
+};
+
+
+const rightButtonConfig = {
+    title: '(Settings)',
+    handler: () => alert('hello!'),
+};
+const leftButtonConfig = {
+    title: '(User)',
+    handler: () => alert('world!'),
+};
+
+const titleConfig = {
+    title: 'UNI/ON',
+};
+
+function ChatComponent(){
+    return (
+	    <View style={styles.chatComp}>
+	    <Text>(Chat Image)</Text>
+	    <Text>Most Recent Sender: Some Text. More text. Wow someones speaking a lot. Lorem Ipsum</Text>
+	    </View>
+    );
+};
+
+
+function ComponentWithNavigationBar() {
+    return (
+	    <View style={styles.container}>
+	        <NavigationBar
+                    title={titleConfig}
+                    rightButton={rightButtonConfig}
+                    leftButton={leftButtonConfig}
+	            containerStyle={styles.navBar}
+	            tintColor="silver"
+	    />
+	    <ChatComponent/>
+	    <ChatComponent/>
+	    <ChatComponent/>
+	    </View>
+    );
+}
+
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <ComponentWithNavigationBar />
   );
 }
