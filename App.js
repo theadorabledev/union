18da541da7e726d35efa23ef0a7455d9d1bb330b
The Hitchhiker <45379459+youwereeatenbyalid@users.noreply.github.com>
2022-06-27 17:24:59 -0400

    Main Screen Components
    MainScreenNavBarComponent
    
    ChatComponent
    
    MessagesListComponent

--- App.js
+++ App.js
@@ -1,87 +1,215 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Button, Image, TouchableOpacity, TouchableHighlight } from "react-native";
 import NavigationBar from 'react-native-navbar';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+//Global Style Variables
+const userProfileSize = 40
+const contactProfileSize = 50
+const highlightcolor = 'deepskyblue'
+const defaultprofile = require('./assets/profilepicsquaresmall.png')
+
+//StyleSheet
 
 const styles = {
     container: {
-	flex: 1,
-	paddingTop: 32,
-	padding: 2,
-	borderRadius: 32,
+		flex: 1,
+		paddingTop: 32,
+		padding: 2,
+		borderRadius: 32,
     },
     chatComp: {
-	borderWidth: 1,
-	maxHeight: 64,
-	padding: 4,
+		flexDirection:'row',
+		flexWrap: "wrap",
+		alignItems:'center',
+		maxHeight: 100,
+		padding: 4,
     },
     miniChat: {
-	flexDirection: 'row',
-	justifyContent: 'space-around',
-	color: "red"
-	
+		flexDirection: 'column',
+		justifyContent: 'space-around',
+		color: "red",
+		padding: 4,
+		paddingTop: 10,
+		paddingBottom: 10,
     },
+
+	profileImage: {
+		width: contactProfileSize,
+		height: contactProfileSize,
+		resizeMode: 'stretch',
+		borderRadius: contactProfileSize/2,
+	},
+	profileButton: {
+		width: contactProfileSize, 
+		height:contactProfileSize, 
+		borderRadius: contactProfileSize/2,
+		alignItems:'center',
+		justifyContent:'center',
+	},
+	
+	
+	userProfileImage: {
+		width: userProfileSize,
+		height: userProfileSize,
+		resizeMode: 'stretch',
+		borderRadius: userProfileSize/2,
+	},
+	
+	userProfileButton: {
+		width: userProfileSize, 
+		height:userProfileSize, 
+		borderRadius: userProfileSize/2,
+		alignItems:'center',
+		justifyContent:'center',
+	},
     userName: {
-	fontWeight: 'bold',
-	fontSize: 16,
-	paddingBottom: 4,
-  },
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+	navBar: {
+		paddingLeft: 4,
+		paddingRight: 4,
+	},
+
 
 };
 
+//user data
 
-const rightButtonConfig = {
-    title: '(Settings)',
-    handler: () => alert('hello!'),
-};
+
+let username = "Test User"
+let userprofilepic = defaultprofile
+
+
+const originalmessages=[
+	{username:"The Fool", message:"Test Message 0. Lorem Ipsum"},
+    {username:"The Magician", message:"Test Message 1. Lorem Ipsum"},
+    {username:"The High Priestess", message:"Test Message 2. Lorem Ipsum"},
+    {username:"The Empress", message:"Test Message 3. Lorem Ipsum"},
+    {username:"The Emperor", message:"Test Message 4. Lorem Ipsum"},
+    {username:" The Hierophant", message:"Test Message 5. Lorem Ipsum"},
+    {username:"The Lovers", message:"Test Message 6. Lorem Ipsum"},
+    {username:"The Chariot", message:"Test Message 7. Lorem Ipsum"},
+    {username:"Strength", message:"Test Message 8. Lorem Ipsum"},
+    {username:"The Hermit", message:"Test Message 9. Lorem Ipsum"},
+    {username:"The Wheel of Fortune", message:"Test Message 10. Lorem Ipsum"},
+	]
+
+
+
+let currentmessages = originalmessages
+
+
+//React components
+
+const SettingsButton = () => {
+	return(
+		<TouchableOpacity style = {styles.userProfileButton} onPress={()=>{alert("Take user to settings")}} >
+			<Ionicons name='settings-outline' size={40} color={highlightcolor}/>
+		</TouchableOpacity>
+	);
+}
+
+
+const ChangeUserIconButton = () => {
+	return(
+		<TouchableHighlight style = {styles.userProfileButton} onPress={()=>{alert("let user change profile picture")}}>
+			<Image
+				style ={styles.userProfileImage}
+				source={userprofilepic}
+			/>
+		</TouchableHighlight>
+	);
+}
+
+
+const ProfileButton = () => {
+	return(
+		<TouchableHighlight style = {styles.profileButton} onPress={()=>{alert("Take user to contact's settings")}}>
+			<Image
+				style ={styles.profileImage}
+				source={defaultprofile}
+			/>
+		</TouchableHighlight>
+	);
+}
+
+
+
 const leftButtonConfig = {
     title: '(User)',
     handler: () => alert('world!'),
 };
 
 const titleConfig = {
-    title: 'UNI/ON',
+    title: username,
 };
 
 function ChatComponent(props){
     return (
-	<View style={styles.chatComp}>
-	    <Text>(Chat Image)</Text>
-	    <View style={styles.miniChat}>
-		<Text style={styles.userName}>{props.username}</Text>
-		<Text>{props.message}</Text>
-	    </View>
-	</View>
+	<TouchableHighlight onPress={()=>{alert("Take user to text chat")}} underlayColor = {highlightcolor}>
+		<View style={styles.chatComp}>
+			<ProfileButton/>
+			<View style={styles.miniChat}>
+				<Text style={styles.userName}>{props.username}</Text>
+				<Text>{props.message}</Text>
+			</View>
+		</View>
+	</TouchableHighlight>
     );
 };
 
-function MainScreenNavBarComponent(){
+function MainScreenNavBarComponent(props){
     return (
 	    <NavigationBar
-                    title={titleConfig}
-                    rightButton={rightButtonConfig}
-                    leftButton={leftButtonConfig}
-	            containerStyle={styles.navBar}
-	            tintColor="silver" />
+			title={titleConfig}
+			rightButton=<SettingsButton/>
+			leftButton=<ChangeUserIconButton/>
+			containerStyle={styles.navBar}
+			tintColor='white'/>
     );
 }
 
+	
+const NoContacts =() =>{
+	return (
+		<View>
+			<Text>It looks like you're all alone.</Text>
+			<Text>Time to Uni/onize!</Text>
+		</View>
+	);
+}
+
+
 function MessagesListComponent(props){
-    const [messages] = useState([
-	{username:"Luca", message:"Test Message 1. Lorem Ipsum"},
-	{username:"Theadora", message:"Test Message 2. Lorem Ipsum"},
-	{username:"Giuliani", message:"Test Message 3. Lorem Ipsum"},
-	{username:"Parva", message:"Test Message 4. Lorem Ipsum"}]);
+    const [messages,setMessages] = useState([]);
+	let empty = (messages.length == 0)
     let messageComponents = messages.map((a, i) => {
 	return <ChatComponent
 		   username={a.username}
 		   message={a.message}
 	       />;
     });
-    return (
-	<ScrollView>
-	    {messageComponents}
-	</ScrollView>
-    );
+		return (
+		<>
+		<Button title="Reset Messages" color = {styles.highlightcolor} onPress={()=>{
+			setMessages(originalmessages);
+			}
+		}/>
+		<Button title="Clear Messages" color = {styles.highlightcolor} onPress={()=>{
+			setMessages([])
+			}
+		}/>
+		{empty ? 
+			<NoContacts/> 
+		:
+			<ScrollView>
+				{messageComponents}
+			</ScrollView>
+		}
+		</>
+		);	
 }
 
 
@@ -89,7 +217,7 @@ function MainScreenComponent(props){
     return (
 	    <View style={styles.container}>
 	        <MainScreenNavBarComponent/>
-		<MessagesListComponent/>
+			<MessagesListComponent/>
 	    </View>
     );
 }
