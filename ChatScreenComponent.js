38d73ed5c97467bd23c6e19315ca1cec1659bb31
Theadora Williams <theadorabledeveloper@gmail.com>
2022-07-14 17:34:14 -0400

    Fixed indentation adn added documentation

--- ChatScreenComponent.js
+++ ChatScreenComponent.js
@@ -12,7 +12,35 @@ import { render } from 'react-dom';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 
+//Styles for the chats
+const ChatStyles = StyleSheet.create({
+    message: {
+	flexDirection: 'row',
+	marginVertical: moderateScale(7,2)
+    },
+    send: {
+	marginLeft: 20,
+    },
+    recieve: {
+	alignSelf: 'flex-end',
+	marginRight: 20
+    },
+    cloud: {
+	maxWidth: moderateScale(250,2),
+	paddingHorizontal: moderateScale(10,2),
+	paddingTop: moderateScale(5,2),
+	paddingBottom: moderateScale(7,2),
+	borderRadius: 20
+    },
+    text: {
+	paddingTop: 3,
+	fontSize: 17,
+	lineHeight: 22
+    }
+})
 
+// Message bubble which displays the text of a message
+// TODO: Add reactions as a state variable.
 const MessageBubble = (props) => {
     return(
 	<View style={[
@@ -46,155 +74,129 @@ const MessageBubble = (props) => {
     )
 }
 
-
+// Container for the messages, updated with state variable, displays "No messages" if so
 const MessageBoxComponent = (props) => {
     const [textmessages,setMessages] = useState(props.messages);
     let empty = (textmessages.length == 0)
     let textComponents = textmessages.map((a, i) => {
-		return <MessageBubble
-			   send
-			   key={i}
-			   text = {a}
-		       />;
+	return <MessageBubble
+		   send
+		   key={i}
+		   text = {a}
+	       />;
     });
-	
     return (
 	<>
-	    {empty ? 
-	    <Text>No messages</Text>
+	    {empty ?
+	     <Text>No messages</Text>
 	     :
 	     <ScrollView>
 		 {textComponents}
 	     </ScrollView>
 	    }
 	</>
-    );	
+    );
 }
 
-
-const ChatStyles = StyleSheet.create({
-	message: {
-		flexDirection: 'row',
-		marginVertical: moderateScale(7,2)
-	},
-	send: {
-		marginLeft: 20,
-	},
-	recieve: {
-		alignSelf: 'flex-end',
-		marginRight: 20
-	},
-	cloud: {
-		maxWidth: moderateScale(250,2),
-		paddingHorizontal: moderateScale(10,2),
-		paddingTop: moderateScale(5,2),
-		paddingBottom: moderateScale(7,2),
-		borderRadius: 20
-	},
-	text: {
-		paddingTop: 3,
-		fontSize: 17,
-		lineHeight: 22
-	}
-})
-
-const KeyboardComponent = () => {
-	const [keyboardStatus, setKeyboardStatus] = useState(undefined);
-  
-	useEffect(() => {
-	  const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
-		setKeyboardStatus("Keyboard Shown");
-	  });
-	  const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
-		setKeyboardStatus("Keyboard Hidden");
-	  });
-  
-	  return () => {
-		showSubscription.remove();
-		hideSubscription.remove();
-	  };
-	}, []);
-  
-	return (
-		<TextInput
-		  style={keyboardStyle.input}
-		  placeholder='Press here…'
-		  onSubmitEditing={Keyboard.dismiss}
-		/>
-	);
-  }
-
+// Styles for the keyboard
 const keyboardStyle = StyleSheet.create({
-	container: {
-	  flex: 1,
-	  justifyContent: 'flex-end',
-	  marginBottom: 10
-	},
-	input: {
-	  padding: 10,
-	  borderWidth: 0.5,
-	  height:50,
-	  borderRadius: 4
-	},
-	status: {
-	  padding: 10,
-	  textAlign: "center"
-	}
+    container: {
+	flex: 1,
+	justifyContent: 'flex-end',
+	marginBottom: 10
+    },
+    input: {
+	padding: 10,
+	borderWidth: 0.5,
+	height:50,
+	borderRadius: 4
+    },
+    status: {
+	padding: 10,
+	textAlign: "center"
+    }
 });
 
+// Displays a keyboard which allows the user to write messages
+// TODO: Connect to messaging API
+const KeyboardComponent = () => {
+    const [keyboardStatus, setKeyboardStatus] = useState(undefined);
 
+    useEffect(() => {
+	const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+	    setKeyboardStatus("Keyboard Shown");
+	});
+	const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+	    setKeyboardStatus("Keyboard Hidden");
+	});
 
+	return () => {
+	    showSubscription.remove();
+	    hideSubscription.remove();
+	};
+    }, []);
 
-const tempContainerStyle ={
-	flex:1,
-	flexDirection: "column",
+    return (
+	<TextInput
+	    style={keyboardStyle.input}
+	    placeholder='Press here…'
+	    onSubmitEditing={Keyboard.dismiss}
+	/>
+    );
 }
 
+const ChatScreenContainerStyle ={
+    flex:1,
+    flexDirection: "column",
+}
 
+// Displays the Chat Screen between two users
 const ChatScreenComponent = ({route, navigation}) => {
-	
-	const titlename = route.params.username.length > 15 ?  route.params.username.substring(0,14) + "..." : route.params.username
-	const chatOptions = [
+
+    const titlename = route.params.username.length > 15 ?  route.params.username.substring(0,14) + "..." : route.params.username
+    const chatOptions = [
 	{text:"Settings", handler:() => navigation.navigate('ChatSettings')},
 	{text:"Search", handler:()=> {alert("Search conversation function")}},
 	{text:"Add to friends", handler:()=> {alert("Add contact to friends list")}},
-	]
+    ]
     React.useLayoutEffect(() => {
 	navigation.setOptions({
 	    title: titlename,
 	    headerRight: () => (
-			<View style={{
-				flexDirection:'row',
-				justifyContent:'space-evenly',
-				alignItems: 'center',
-				minWidth: 80,
-			}}>
-				<SettingsButton onPress={() => navigation.navigate('ChatSettings')}/>
-				<ContextMenu options={chatOptions}ionicon="menu"/>
-			</View>
+		// Settings Button
+		<View style={{
+			  flexDirection:'row',
+			  justifyContent:'space-evenly',
+			  alignItems: 'center',
+			  minWidth: 80,
+		      }}>
+		    <SettingsButton onPress={() => navigation.navigate('ChatSettings')}/>
+		    <ContextMenu options={chatOptions}ionicon="menu"/>
+		</View>
 	    ),
-		headerLeft:()=>(
-			<View style={{
-				flexDirection:'row',
-				flexWrap: "wrap",
-				justifyContent:'flex-start',
-				alignItems: 'center',
-				minWidth: 30,
-				paddingRight: 5,
-			}}>
-				<HeaderBackButton onPress={()=>{navigation.goBack()}}/>
-				<ProfileButton profileSize={GlobalStyle.userProfileSize} profileSource={GlobalStyle.defaultprofile} onPress={()=>{alert("let user change contact's picture")}}/>
-			</View>
-		),
+	    headerLeft:()=>(
+		// Back Button
+		<View style={{
+			  flexDirection:'row',
+			  flexWrap: "wrap",
+			  justifyContent:'flex-start',
+			  alignItems: 'center',
+			  minWidth: 30,
+			  paddingRight: 5,
+		      }}>
+		    <HeaderBackButton onPress={()=>{navigation.goBack()}}/>
+		    <ProfileButton profileSize={GlobalStyle.userProfileSize} profileSource={GlobalStyle.defaultprofile} onPress={()=>{alert("let user change contact's picture")}}/>
+		</View>
+	    ),
 	});
     }, [navigation]);
     const {username, messages} = route.params;
-		return (
-		<View style={tempContainerStyle}>
+    return (
+	<View style={ChatScreenContainerStyle}>
 	    <MessageBoxComponent messages={messages}/>
-		<KeyboardComponent />
-		</View>
-    	);
+	    <KeyboardComponent />
+	</View>
+    );
 
 }
 
